7cd36d471aec86b785145a3feddb08290955b6eb
Fenix <arhange1l@yandex.ru>
2021-02-18 01:17:15 +0300

    Задание 2_3

---
+++ GB/Lesson2_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lesson2_3
+{
+    //Определить, является ли введённое пользователем число чётным.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое число");
+            long Value = long.Parse(Console.ReadLine());
+            if (Value == 0)
+            {
+                Console.WriteLine($"Вы вели {Value}");
+            }
+            else if (Value % 2 == 0)
+                Console.WriteLine("Вы вели четное число");
+            else
+                Console.WriteLine("Вы вели не четное число");
+        }
+    }
+}
