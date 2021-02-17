38ebfde1df7758e767dac832e0655498e04e3f43
Fenix <arhange1l@yandex.ru>
2021-02-17 23:05:34 +0300

    Задание 2_1

---
+++ GB/Lesson2_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lesson2_1
+{
+   // Запросить у пользователя минимальную и максимальную температуру за сутки и вывести среднесуточную температуру.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вычисление среднесуточной температуры за сутки");
+            Console.WriteLine("Введите минимальное значение температуры");
+            int minValue = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите максимальное значение температуры");
+            int maxValue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Среднесуточная температура = {(minValue + maxValue) / 2}°");
+
+        }
+    }
+}
