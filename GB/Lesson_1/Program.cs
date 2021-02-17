d7a50b7f1616f22e1c1fe338f5e1e5914a9ee303
Fenix <arhange1l@yandex.ru>
2021-02-17 22:16:25 +0300

    Перенос всего проекта в Git

---
+++ GB/Lesson_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime(2021, 02, 18);
+
+            Console.WriteLine("Как вас зовут?");
+            string name = Console.ReadLine();
+            if (name.ToLower() == "андрей кучма")
+                Console.WriteLine($"Здравствуй сансей! Не забудьте, что следующее занятие состоится [{date.ToLongDateString()}].");
+            else
+                Console.WriteLine($"Привет {name}! Сегодня [{DateTime.Now}].");
+        }
+    }
+}
