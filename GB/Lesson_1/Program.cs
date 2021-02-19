a4a5f3d9847a9f423bd978036da0e2b32092d689
inWork <12>
2021-02-19 13:44:27 +0300

    1

--- GB/Lesson_1/Program.cs
+++ GB/Lesson_1/Program.cs
@@ -6,14 +6,7 @@ namespace GB
     {
         static void Main(string[] args)
         {
-            DateTime date = new DateTime(2021, 02, 18);
 
-            Console.WriteLine("Как вас зовут?");
-            string name = Console.ReadLine();
-            if (name.ToLower() == "андрей кучма")
-                Console.WriteLine($"Здравствуй сансей! Не забудьте, что следующее занятие состоится [{date.ToLongDateString()}].");
-            else
-                Console.WriteLine($"Привет {name}! Сегодня [{DateTime.Now}].");
         }
     }
 }
