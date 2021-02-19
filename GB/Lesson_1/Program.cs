f3f60c62e0b251ed843ce54fe369eca8db3453c7
Fenix <arhange1l@yandex.ru>
2021-02-19 21:38:52 +0300

    Задание 2_6

--- GB/Lesson_1/Program.cs
+++ GB/Lesson_1/Program.cs
@@ -8,13 +8,13 @@ namespace GB
     {
         enum DayOfWeek
         {
-            Monday = 0b_1000000,
-            Tuesday = 0b_0100000,
-            Wednesday = 0b_0010000,
-            Thursday = 0b_0001000,
-            Friday = 0b_0000100,
-            Saturday = 0b_0000010,
-            Sunday = 0b_0000001,
+            Monday = 0b_00_00001,
+            Tuesday = 0b_00_00010,
+            Wednesday = 0b_00_00100,
+            Thursday = 0b_00_01000,
+            Friday = 0b_00_10000,
+            Saturday = 0b_01_00000,
+            Sunday = 0b_10_00000,
         }
 
     static void Main(string[] args)
@@ -22,14 +22,21 @@ namespace GB
             byte officeOne = 0b_01111_00;
             int officeTwo = 0b_11111_11;
 
-                foreach (DayOfWeek day in Enum.GetValues(typeof(DayOfWeek)))
-                {
-                    if((officeOne & (byte)day) == (byte)day)
-                {
-                    Console.WriteLine((DayOfWeek)day);
-                }
-                }
-                
+
+            Console.Write($"Офис 1 работает - :") ;
+            foreach (DayOfWeek day in Enum.GetValues(typeof(DayOfWeek)))
+            {
+                if ((officeOne & (byte)day) == (byte)day)
+                    Console.Write($" {(DayOfWeek)day} :");
+            }
+
+            Console.Write($"\nОфис 2 работает - :");
+            foreach (DayOfWeek day in Enum.GetValues(typeof(DayOfWeek)))
+            {
+                if ((officeTwo & (byte)day) == (byte)day)
+                    Console.Write($" {(DayOfWeek)day} :");
+            }
+
 
 
 
